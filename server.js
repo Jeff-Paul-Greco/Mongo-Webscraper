ae19076a5db6f915d8e4d0474107eb8ad846e2c8
Jeff Greco <jeffpgreco@gmail.com>
2019-11-20 14:59:25 -0500

    added reddit scrape

--- server.js
+++ server.js
@@ -26,8 +26,46 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlin
 
 mongoose.connect(MONGODB_URI);
 
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://www.reddit.com/").then(function (response) {
+
+        var $ = cheerio.load(response.data);
+
+        $("h3").each(function (i, element) {
+
+            var result = {};
+
+            result.title = $(this)
+                .text();
+            result.link = $(this)
+                .parent()
+                .parent("a")
+                .attr("href");
+            result.description = $(this)
+                .text();
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+
+                    console.log(err);
+                });
+        });
+
+        res.send("Scrape Complete");
+    });
+});
+
 app.get("/", function (err, res) {
     res.render("index");
+});
+
+app.get("*", function (req, res) {
+    res.render("404");
 });
 
 app.listen(PORT, function () {
